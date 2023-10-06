0d655ec8a8f4f5c6356512670a810bd93655b66c
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-05 21:18:47 -0600

    Remove solution

--- contar_vocales.py
+++ contar_vocales.py
@@ -1,28 +1,15 @@
-# -*- coding: utf-8 -*-
 """
-Escribir un programa que cuente la cantidad de vocales en una frase
+Inserta el encabezado aquí y escribe tu código abajo
+"""
 
-Paso 0. Entender con un ejemplo
-Ejemplo: ¿Cuántas vocales hay en la frase: "Hola, mundo."?
-Respuesta: Hay 4 vocales: la "o" y la "a" de "Hola" y la "u" 
-           y la "o" de "mundo".
-           
-Paso 1. Análisis...
-"""
 # Declaraciones
-VOCALES = "aeiouáéíóúü"
+CONSTANTE = valor
 
 # Entradas
-frase = input("Frase: ")
+entrada = input()
 
 # Proceso
-posicion = 0
-contar_vocales = 0
-while posicion < len(frase):
-    caracter = frase[posicion].lower()
-    if caracter in VOCALES:
-        contar_vocales += 1
-    posicion += 1
+
 
 # Salidas
-print(f"La frase tiene {contar_vocales} vocales.")
+print(salida)
